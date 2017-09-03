c571423340247c391b643acf70793bed57f6bb3a
ramonsantos <ramonsantos.pe@gmail.com>
2017-09-03 13:46:56 -0300

    Chapter 9.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,10 +6,21 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._negociacoes = new Negociacoes(model => {
-      console.log(this);
+    const self = this;
 
-      this._negociacoesView.update(model);
+    this._negociacoes = new Proxy(new Negociacoes(), {
+      get(target, prop, receiver){
+        if(typeof(target[prop]) == typeof(Function)
+            && ['adiciona', 'esvazia'].includes(prop)) {
+          return function() {
+            console.log(`"${prop}" disparou a armadilha`);
+            target[prop].apply(target, arguments);
+            self._negociacoesView.update(target);
+          }
+        } else {
+          return target[prop];
+        }
+      }
     });
 
     this._negociacoesView = new NegociacoesView('#negociacoes');
