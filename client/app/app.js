9fe8cc4a80fc19923660951b9c334f2229639c4f
ramonsantos <ramonsantos.pe@gmail.com>
2017-09-03 11:20:42 -0300

    Chapter 8.

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,7 @@
 const controller = new NegociacaoController();
 
-document
-    .querySelector('.form')
-        .addEventListener('submit', controller.adiciona.bind(controller));
+document.querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
+
+document.querySelector('#botao-apaga')
+    .addEventListener('click', controller.apaga.bind(controller));
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,15 +1,20 @@
 class NegociacaoController {
   constructor() {
     const $ = document.querySelector.bind(document);
-    
+
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    
-    this._negociacoes = new Negociacoes();
+
+    this._negociacoes = new Negociacoes(model => {
+      console.log(this);
+
+      this._negociacoesView.update(model);
+    });
+
     this._negociacoesView = new NegociacoesView('#negociacoes');
     this._negociacoesView.update(this._negociacoes);
-    
+
     this._mensagem = new Mensagem();
     this._mensagemView = new MensagemView('#mensagemView');
     this._mensagemView.update(this._mensagem);
@@ -17,20 +22,23 @@ class NegociacaoController {
 
   adiciona(event) {
     event.preventDefault();
-    
+
     this._negociacoes.adiciona(this._criaNegociacao());
     this._mensagem.texto = 'Negociação adicionada com sucesso!';
-    this._negociacoesView.update(this._negociacoes);
-
-    this._limpaFormulario();
     this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
+  }
+
+  apaga() {
+    this._negociacoes.esvazia();
+    this._mensagem.texto = 'Negociações apagadas com sucesso!';
+    this._mensagemView.update(this._mensagem);
   }
 
   _limpaFormulario() {
     this._inputData.value = '';
     this._inputQuantidade.value = 1;
-    this._inputValor.value = 0.0
+    this._inputValor.value = 0.0;
     this._inputData.focus();
   }
 
