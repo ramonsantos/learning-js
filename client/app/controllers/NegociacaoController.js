a0aa376952cf9c0ba96f104059ca5e7b90ba71d5
ramonsantos <ramonsantos.pe@gmail.com>
2017-09-04 16:54:12 -0300

    Chapter 11.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -20,11 +20,22 @@ class NegociacaoController {
   }
 
   adiciona(event) {
-    event.preventDefault();
+    try {
+      event.preventDefault();
 
-    this._negociacoes.adiciona(this._criaNegociacao());
-    this._mensagem.texto = 'Negociação adicionada com sucesso!';
-    this._limpaFormulario();
+      this._negociacoes.adiciona(this._criaNegociacao());
+      this._mensagem.texto = 'Negociação adicionada com sucesso!';
+      this._limpaFormulario();
+    } catch(err) {
+      console.log(err);
+      console.log(err.stack);
+
+      if (err instanceof DataInvalidaException) {
+        this._mensagem.texto = err.message;
+      } else {
+        this._mensagem.texto = 'Um erro não esperado aconteceu. Entre em contato com o suporte';
+      }
+    }
   }
 
   apaga() {
