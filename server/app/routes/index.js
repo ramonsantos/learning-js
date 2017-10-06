762b8938218b752cc09a2fd18a9486d803b79eeb
ramonsantos <ramonsantos.pe@gmail.com>
2017-10-06 10:12:22 -0300

    Add server.

---
+++ server/app/routes/index.js
@@ -0,0 +1,15 @@
+var api = require('../api');
+
+module.exports  = function(app) {
+  app.route('/negociacoes/semana')
+      .get(api.listaSemana);
+
+  app.route('/negociacoes/anterior')
+      .get(api.listaAnterior);
+
+  app.route('/negociacoes/retrasada')
+      .get(api.listaRetrasada);
+
+  app.route('/negociacoes')
+      .post(api.cadastraNegociacao);
+};
